c9510599828bf0ad9ab3cb5bd4521b9feccf7b51
Mohssine Falih <mofal100@uni-duesseldorf.de>
2023-12-19 14:37:30 +0100

    Initialized project structure and configuration

---
+++ oop/anemic/src/main/java/de/hhu/anemic/warenkorb/WarenkorbApplication.java
@@ -0,0 +1,13 @@
+package de.hhu.anemic.warenkorb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarenkorbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarenkorbApplication.class, args);
+	}
+
+}
---
+++ oop/rich/src/test/java/de/hhu/rich/warenkorb/WarenkorbApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hhu.rich.warenkorb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarenkorbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
